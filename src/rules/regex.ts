d8a8760a36e586850adf4caab0e735e4c1efbdb5
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-10-22 11:52:49 +0330

    regex validator util added

---
+++ src/rules/regex.ts
@@ -0,0 +1,30 @@
+import { curry } from "ramda";
+import { validatorBuilder } from "../core/builder";
+import { BuilderConfig } from "../types/BuilderConfig";
+import { RegexConfig } from "../types/RegexConfig";
+
+const regexValidatorRaw = (
+  config: RegexConfig | string | RegExp,
+  input: string
+) => {
+  function isConfig(toBeDetermined: any): toBeDetermined is RegexConfig {
+    if (toBeDetermined as RegexConfig) {
+      return true;
+    }
+    return false;
+  }
+  const regexPattern: RegExp = isConfig(config)
+    ? new RegExp(config.pattern)
+    : new RegExp(config);
+  const message: string =
+    isConfig(config) && config.message
+      ? config.message
+      : `Input format is wrong`;
+  const builderConfig: BuilderConfig = {
+    validator: (value: string) => regexPattern.test(value),
+    message,
+  };
+  return validatorBuilder(builderConfig, input);
+};
+
+export const regexValidator = curry(regexValidatorRaw);
